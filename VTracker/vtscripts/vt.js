e721742759a3842657186f664b9fd0f361b3f47e
Raj Kiran Singh <rajkiran.singh@rudrasofttech.com>
2019-03-29 20:45:50 +0530

    f

--- VTracker/vtscripts/vt.js
+++ VTracker/vtscripts/vt.js
@@ -262,41 +262,34 @@ var VisitTracker = /** @class */ (function () {
             browserName = "Opera";
             fullVersion = nAgt.substring(verOffset + 4);
         }
-        // In older Opera, the true version is after "Opera" or after "Version"
         else if ((verOffset = nAgt.indexOf("Opera")) != -1) {
             browserName = "Opera";
             fullVersion = nAgt.substring(verOffset + 6);
             if ((verOffset = nAgt.indexOf("Version")) != -1)
                 fullVersion = nAgt.substring(verOffset + 8);
         }
-        // In MSIE, the true version is after "MSIE" in userAgent
         else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
             browserName = "Microsoft Internet Explorer";
             fullVersion = nAgt.substring(verOffset + 5);
         }
-        // In Edge, the true version is after "MSIE" in userAgent
         else if ((verOffset = nAgt.indexOf("Edge")) != -1) {
             browserName = "Edge";
             fullVersion = nAgt.substring(verOffset + 5);
         }
-        // In Chrome, the true version is after "Chrome" 
         else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
             browserName = "Chrome";
             fullVersion = nAgt.substring(verOffset + 7);
         }
-        // In Safari, the true version is after "Safari" or after "Version" 
         else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
             browserName = "Safari";
             fullVersion = nAgt.substring(verOffset + 7);
             if ((verOffset = nAgt.indexOf("Version")) != -1)
                 fullVersion = nAgt.substring(verOffset + 8);
         }
-        // In Firefox, the true version is after "Firefox" 
         else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
             browserName = "Firefox";
             fullVersion = nAgt.substring(verOffset + 8);
         }
-        // In most other browsers, "name/version" is at the end of userAgent 
         else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
             (verOffset = nAgt.lastIndexOf('/'))) {
             browserName = nAgt.substring(nameOffset, verOffset);
